587183d1748c06ed310a39b030ad0d7350a9af5b
BurnEtz <32961116+burnetz@users.noreply.github.com>
2019-07-18 01:40:21 +0900

    2-9 list9まで終了

---
+++ 2-9/list9.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+	answer: '',
+
+	options: [
+	    {code: 'ans1', label: '初めて'},
+	    {code: 'ans2', label: '週１回以上'},
+	    {code: 'ans3', label: '週２回以上'},
+	    {code: 'ans4', label: '半年に１回'},
+	],
+    }
+});
