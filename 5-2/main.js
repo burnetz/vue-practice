cdfdc9d80bce0eb45e6acb0d1b23e5dabf75b768
burnetz <32961116+burnetz@users.noreply.github.com>
2020-03-01 01:43:37 +0900

    日付のコントロールに関する処理を追加

--- 5-2/main.js
+++ 5-2/main.js
@@ -24,9 +24,19 @@ app.querySelector('#opt4').addEventListener('change', onInputChanged, false);
 
 //ページの読み込みが完了したときに呼び出されるイベントハンドラ
 function onPageLoad(event) {
+    //フォームコントロールを取得
+    var wedding_date = app.querySelector('#wedding_date'); //挙式日
+    var delivery_date = app.querySelector('#delivery_date'); //DVD仕上がり予定日
+    //今日の日付を取得
+    var dt = new Date();
     //挙式日に2ヶ月後の日付を設定
+    dt.setMonth(dt.getMonth() + 2);
+    wedding_date.value = formatDate(dt);
     //DVD仕上がり予定日に挙式日の1週間前の日付を設定
+    dt.setDate(dt.getDate() - 7);
+    delivery_date.value = formatDate(dt);
     //DVD仕上がり予定日に翌日以降しか入力できないようにする
+    delivery_date.setAttribute('min', tomorrow());
     //フォームの表示を更新する
     updateForm();
 }
@@ -38,6 +48,21 @@ function onInputChanged(event) {
 }
 
 /* 関数 */
+
+//日付をYYYY-MM-DDの書式で返すメソッド
+function formatDate(dt) {
+    var y = dt.getFullYear();
+    var m = ('00' + (dt.getMonth() + 1)).slice(-2);
+    var d = ('00' + dt.getDate()).slice(-2);
+    return (y + '-' + m + '-' + d);
+}
+
+//明日の日付をYYYY-MM-DDの書式で返すメソッド
+function tomorrow() {
+    var dt = new Date();
+    dt.setDate(dt.getDate() + 1);
+    return formatDate(dt);
+}
 
 //金額の表示を更新する関数
 function updateForm() {
